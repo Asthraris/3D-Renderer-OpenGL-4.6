82c7bc7af968209f657b6139287b98fd01cd0fc6
justforfun <asthraris@gmail.com>
2024-12-27 17:24:10 +0530

    Added pyramid decalation

--- src/myClasses/headers/genShape.h
+++ src/myClasses/headers/genShape.h
@@ -4,9 +4,10 @@ class genShape
 {
 public:
     static void deleteShapeData(shapeDATA& data);
-	static shapeDATA genTRIANGLE();
+    static shapeDATA genTRIANGLE();
     static shapeDATA genSQUARE();
     static shapeDATA genCUBE();
+    static shapeDATA genPYRAMID();
  
 };
 
